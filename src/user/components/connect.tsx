c2534a6dde57d2571025bcfd24e6992f1fa08054
owopr <owo.pre.eno@gmail.com>
2024-11-17 16:59:08 +0100

    connect responsive

--- src/user/components/connect.tsx
+++ src/user/components/connect.tsx
@@ -4,9 +4,9 @@ const Connect = () => {
   const { connectors, connect } = useConnect();
 
   return (
-    <section className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm">
-      <div className="w-1/3 space-y-8 rounded-2xl border border-black/5 bg-white p-8 text-center shadow-xl">
-        <h1 className="text-4xl font-semibold">Connect a wallet</h1>
+    <section className="fixed inset-0 z-50 flex items-center justify-center p-4 backdrop-blur-sm">
+      <div className="w-full space-y-8 rounded-2xl border border-black/5 bg-white p-4 py-8 text-center shadow-xl md:w-1/3 md:p-8">
+        <h1 className="text-2xl font-semibold md:text-4xl">Connect a wallet</h1>
         <div className="flex flex-col items-start gap-4">
           {connectors.map((connector) => {
             if (connector.id != "injected") {
@@ -15,16 +15,22 @@ const Connect = () => {
                   key={connector.uid}
                   onClick={() => connect({ connector })}
                   type="button"
-                  className="flex w-full items-center justify-center gap-4 rounded-full border py-2.5 text-left font-semibold"
+                  className="flex w-full items-center justify-center gap-4 text-nowrap rounded-full border px-4 py-2.5 text-left font-semibold"
                 >
-                  <span className="flex aspect-square w-8 items-center justify-center">
-                    <img
-                      src={`./${connector.id}.svg`}
-                      alt={connector.name}
-                      className="w-full"
-                    />
+                  {(connector.id == "coinbaseWalletSDK" ||
+                    connector.id == "io.metamask" ||
+                    connector.id == "walletConnect") && (
+                    <span className="flex aspect-square w-8 items-center justify-center">
+                      <img
+                        src={`./${connector.id}.svg`}
+                        alt={connector.name}
+                        className="w-full"
+                      />
+                    </span>
+                  )}
+                  <span className="w-1/3 text-sm md:text-base">
+                    {connector.name}
                   </span>
-                  <span className="w-1/3">{connector.name}</span>
                 </button>
               );
             }
